6b053955e075a9de8dc1ee32e232a27783219476
michaelporterjr44-bit <michaelporterjr44@gmail.com>
2026-08-08 18:05:56 +0300

    fix: fix the hierit of the PayrollEmployee

--- src/types/employee/payrollEmployee.ts
+++ src/types/employee/payrollEmployee.ts
@@ -3,5 +3,5 @@ import { Employee } from "./employee";
 export interface PayrollEmployee extends Employee {
   baseSalary: number; // Salaire de base
   netToPay: number;   // Net à payer
-  status: "Validé" | "En cours"; // Statut de la paie
+  payrollStatus: "Validé" | "En cours"; // Statut de la paie
 }
