ac4291c5501f139f59d16a4d4941e37dcd8bd61d
michaelporterjr44-bit <michaelporterjr44@gmail.com>
2026-08-08 18:18:17 +0300

    fix: payroll employee status

--- src/types/employee/payrollEmployee.ts
+++ src/types/employee/payrollEmployee.ts
@@ -1,7 +1,7 @@
 import { Employee } from "./employee";
 
 export interface PayrollEmployee extends Employee {
-  baseSalary: number; // Salaire de base
-  netToPay: number;   // Net à payer
-  payrollStatus: "Validé" | "En cours"; // Statut de la paie
+  baseSalary: number;
+  netToPay: number;
+  payrollStatus: "Validé" | "En cours";
 }
